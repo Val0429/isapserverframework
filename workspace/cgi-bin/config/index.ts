814872891aebe74dcc872239e26eabcaf1877963
Val.Liu <Val.Liu@isapsolution.com>
2018-06-01 11:42:53 +0800

    config fully configurable

--- workspace/cgi-bin/config/index.ts
+++ workspace/cgi-bin/config/index.ts
@@ -103,21 +103,46 @@ async function updateConfig(key: string, data: object) {
         }
         if (result) break;
     }
-    console.log('result?', result);
-    if (!result) throw "should not happen";
+    if (!result) throw Errors.throw(Errors.Custom, [`config path not found <${key}>`]);
 
     /// 2) read file
     var content: string = await (promisify(fs.readFile) as any)(result, "UTF-8");
-    console.log("content?", content);
 
     /// 2) match export default {0}
     var regex = /export default ([^\s;]+)/;
     var matches = content.match(regex);
-    console.log("matches", matches);
-    
+    if (matches.length < 2) throw Errors.throw(Errors.Custom, [`config path format error <${key}>`]);
+    var token = matches[1];
 
-    /// 3) match {0} with var {0}: any = { }. get inside braclets as {1}
+    /// 3) match {0} with var {0}: any = { }.
+    var regex = new RegExp(` ${token}[\: \=]`);
+    var found = content.search(regex);
+    if (found < 0) throw Errors.throw(Errors.Custom, [`config path format error <${key}>`]);
 
-    /// 4) replace {1} with data
+    /// 4) get content of 3) inside braclets as {1}
+    var start, end, ct = 1;
+    for (var i = found+1; i<content.length; ++i) {
+        var tok = content[i];
+        if (!start) { if (tok === '{') start = i; continue; }
+        switch (tok) { case '{': ct++; break; case '}': ct--; break; default: break; }
+        if (ct === 0) { end = i+1; break; }
+    }
+    if (!start || !end) throw Errors.throw(Errors.Custom, [`config path format error <${key}>`]);
+    var innerContent = content.substring(start, end);
+    var parser = (input: string): object => {
+        /// remove trailing comma
+        return eval(`(${input})`);
+    }
+    var innerObject = parser(innerContent);
 
+    /// 5) replace {1} with data
+    innerObject = { ...innerObject, ...data };
+    var prettifier = (input: string): string => {
+        var regex = /"([^"]+)":/mg;
+        return input.replace(regex, (a, b) => b+':');
+    }
+    var final = `${content.substring(0, start)}${prettifier(JSON.stringify(innerObject, undefined, 4))}${content.substring(end)}`;
+
+    /// 6) write back
+    await (<any>promisify(fs.writeFile))(result, final);
 }

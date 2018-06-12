05cbe643dbecd399756e9c3a2fac6e0557be30b2
Val <val.liu@isapsolution.com>
2018-06-12 16:14:47 +0800

    prepare add tuner to C,R,U,D

--- models/schedulers/schedulers.base.ts
+++ models/schedulers/schedulers.base.ts
@@ -1,5 +1,8 @@
 import { IEvent } from './../events/events.base';
+import { ParseObject } from './../../helpers/parse-server/parse-helper';
 
 export class Schedulers {
-
+    event: ParseObject<IEvent>;
+    time: any;
+    action: any;
 }

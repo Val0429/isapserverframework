cddf0a8eef2bc3d87e2d19112974704e8b8e9d28
Val <val.liu@isapsolution.com>
2018-08-13 14:07:49 +0800

    add semaphore support

---
+++ helpers/utility/semaphore.ts
@@ -0,0 +1,32 @@
+export class Semaphore {
+    private max: number;
+    private count: number;
+    private promises: any[] = [];
+    private completed: any[] = [];
+
+    constructor(max: number) {
+        this.max = this.count = max;
+    }
+    public toPromise(): Promise<void> {
+        if (this.count > 0) { --this.count; return Promise.resolve(); }
+        let result;
+        let promise = new Promise<void>((resolve) => result = resolve);
+        this.promises.push(result);
+        return promise;
+    }
+    public release() {
+        var resolve = this.promises.shift();
+        if (resolve === undefined) {
+            this.count = Math.min(++this.count, this.max);
+            if (this.count === this.max) while (this.completed.length > 0) (this.completed.shift())();
+            return;
+        }
+        resolve();
+    }
+    public onCompleted(): Promise<void> {
+        let result;
+        let promise = new Promise<void>((resolve) => result = resolve);
+        this.completed.push(result);
+        return promise;
+    }
+}

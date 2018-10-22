ce968bc53e76dc96124366f74c21ade0a55fe07c
Val <val.liu@isapsolution.com>
2018-10-22 11:17:20 +0800

    add comment into auto-indexed file, make it clear

--- helpers/shells/auto-index.ts
+++ helpers/shells/auto-index.ts
@@ -42,6 +42,11 @@ export function autoIndex(path: string) {
     }
     let data = templates.filter( (p) => p ).join("\r\n");
     if (!data) data = "export default undefined;";
+    data = `/**
+ * ALARM: DON'T MODIFY THIS FILE.
+ * this file is auto created. whatever you insert into this file will be overwritten.
+ */
+${data}`;
 
     /// 2) compare with current index file
     let indexPath = `${path}/${indexName}.ts`;
--- models/schedulers/actions/index.ts
+++ models/schedulers/actions/index.ts
@@ -1,3 +1,7 @@
+/**
+ * ALARM: DON'T MODIFY THIS FILE.
+ * this file is auto created. whatever you insert into this file will be overwritten.
+ */
 export * from './core';
 export * from './email';
 export * from './sgsms';

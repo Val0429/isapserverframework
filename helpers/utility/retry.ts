5eb4becec44ce9f74570affd47e5dd317fc7f706
Val <val.liu@isapsolution.com>
2018-12-13 14:35:55 +0800

    retry error report

--- helpers/utility/retry.ts
+++ helpers/utility/retry.ts
@@ -1,9 +1,11 @@
+import { Log } from "./log";
+
 export async function retry<T>(
     func: (
         resolve: (value?: T | PromiseLike<T>) => void,
         reject: (reason?: any) => void
     ) => void | Promise<void>,
-    times: number = 10 ): Promise<T> {
+    times: number = 10, hintname: string = null ): Promise<T> {
 
     let count: number = 0;
     let err;
@@ -14,6 +16,11 @@ export async function retry<T>(
             } catch(e) {
                 err = e;
             }
+
+            /// error log
+            let test = Math.log10(count);
+            if (test > 0 && Number.isInteger(test)) Log.Error("Critical Error", `${hintname ? `Function <${hintname}> ` : ''}Retry ${count} times with error: ${err}`);
+
         } while(times === 0 || (++count < times));
         return reject(err);
     });

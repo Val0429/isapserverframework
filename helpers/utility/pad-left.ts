21cf0e0585853d6d18dce21939b24d3e0f113aa6
Val <val.liu@isapsolution.com>
2019-01-08 10:54:56 +0800

    fix timestamp milliseconds multiple zero problem

--- helpers/utility/pad-left.ts
+++ helpers/utility/pad-left.ts
@@ -1,4 +1,4 @@
 export function padLeft(input: string | number, digits: number, letter: string = "0"): string {
     if (typeof input === 'number') input = input + "";
-    return input.length >= digits ? input : Array(digits-input.length).fill(letter) + input;
+    return input.length >= digits ? input : Array(digits-input.length).fill(letter).join("") + input;
 }

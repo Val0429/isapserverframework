c7c2ce2b7d37ff3e460ed6204ae849fbec5f5cb5
Val <val.liu@isapsolution.com>
2019-11-04 12:13:11 +0800

    npm run clean, will now clean for windows service as well

--- shells/clean-cache.shell.ts
+++ shells/clean-cache.shell.ts
@@ -8,12 +8,15 @@ const os = require('os');
 const fs = require('fs');
 const rimraf = require('rimraf');
 
-let path = os.tmpdir();
-fs.readdir(path, (err, filenames) => {
-    let regex = /^ts-node/i;
-    for (let filePath of filenames) {
-        if (regex.test(filePath)) {
-            rimraf.sync(`${path}/${filePath}`);
+let paths = [os.tmpdir(), "C:\\Windows\\Temp\\"];
+
+paths.forEach( (path) => {
+    fs.readdir(path, (err, filenames) => {
+        let regex = /^ts-node/i;
+        for (let filePath of filenames) {
+            if (regex.test(filePath)) {
+                rimraf.sync(`${path}/${filePath}`);
+            }
         }
-    }
+    });
 });

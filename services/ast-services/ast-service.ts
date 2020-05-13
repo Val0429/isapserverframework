4cf40a7a89f57ee57cd83bbb8bb3ecca2e4267da
Min.Hsieh <Min.Hsieh@isapsolution.com>
2020-05-13 15:59:29 +0800

    Upgrade Ast Service update to trim when value was string

--- services/ast-services/ast-service.ts
+++ services/ast-services/ast-service.ts
@@ -574,7 +574,7 @@ namespace AstConverter {
     export function toString(input: string, name: string, isArray: boolean = false): string {
         if (typeof input !== 'string') throw Errors.throw(Errors.CustomInvalid, [`<${name}> should be string${isArray?'[]':''}.`]);
         if (!canBeEmpty && input.length === 0) throw Errors.throw(Errors.CustomInvalid, [`${name} should not be an empty string.`]);
-        return input;
+        return input.trim();
     }
 
     export function toNumber(input: string | number, name: string, isArray: boolean = false): number {

80169dbf87689a80a647dec279e52bc9167f870a
Min.Hsieh <a1991704@yahoo.com.tw>
2020-07-21 11:40:15 +0800

    Upgrade Log helper change to use print module

--- helpers/utility/log.ts
+++ helpers/utility/log.ts
@@ -5,13 +5,16 @@
  */
 
 import 'colors';
-import { padLeft } from './pad-left';
 let caller = require('caller');
+
+import { PrintService } from 'helpers';
+
 
 export enum Level {
     Trace = 1,
     Info = 2,
-    Error = 3
+    Success = 4,
+    Error = 3,
 }
 
 export namespace Log {
@@ -22,75 +25,70 @@ export namespace Log {
         currentRegEx = typeof reg === 'string' ? new RegExp(reg) : reg;
     }
 
-    function timestamp(): string {
-        let now = new Date();
-        return `${'['.grey}` +
-               `${now.getMonth()+1}/${padLeft(now.getDate(),2)} ${padLeft(now.getHours(),2)}:${padLeft(now.getMinutes(),2)}:${padLeft(now.getSeconds(),2)}.${padLeft(now.getMilliseconds(),3)}`.cyan +
-               `${']'.grey} `;
-    }
-
     function TestPass(level: Level): boolean {
-        if (
-            level>=currentLevel &&
-            (!currentRegEx || currentRegEx && currentRegEx.test(caller(2)))
-        ) return true;
+        if (level>=currentLevel && (!currentRegEx || currentRegEx && currentRegEx.test(caller(2)))) return true;
         return false;
     }
 
-    let getTraceMessage = (title: string, message: string, withTimestamp: boolean = true) => `${withTimestamp?timestamp():''}${"<".grey}${title.white}${">".grey} ${message}`;
-    let getInfoMessage = (title: string, message: string, withTimestamp: boolean = true) => `${withTimestamp?timestamp():''}${"<".magenta}${title.yellow}${">".magenta} ${message}`;
-    let getErrorMessage = (title: string, message: string, withTimestamp: boolean = true) => `${withTimestamp?timestamp():''}${"<".red}${title.white.bgRed}${">".red} ${message}`;
-
-    export function Trace(title: string, message: string) {
+    export function Trace(title: string, message: string): void {
         if (!TestPass(Level.Trace)) return;
-        let msg = getTraceMessage(title, message);
-        console.log(msg);
-        return msg;
+
+        PrintService.log(`${!title ? '' : `<${title}> `}${message}`, undefined, 'message');
     }
 
-    export function Info(title: string, message: string) {
+    export function Info(title: string, message: string): void {
         if (!TestPass(Level.Info)) return;
-        let msg = getInfoMessage(title, message);
-        console.log(msg);
-        return msg;
+
+        PrintService.log(`${!title ? '' : `<${title}> `}${message}`, undefined, 'info');
     }
 
-    export function Error(title: string, message: string) {
+    export function Success(title: string, message: string): void {
+        if (!TestPass(Level.Success)) return;
+
+        PrintService.log(`${!title ? '' : `<${title}> `}${message}`, undefined, 'success');
+    }
+
+    export function Error(title: string, message: string): void {
         if (!TestPass(Level.Error)) return;
-        let msg = getErrorMessage(title, message);
-        console.log(msg);
-        return msg;
+
+        PrintService.log(`${!title ? '' : `<${title}> `}${message}`, undefined, 'error');
     }
 
     class TimeEndWrapper {
-        private msg: string;
-        constructor(msg: string) {
-            this.msg = msg;
+        private _title: string = '';
+        private _message: string = '';
+        private _type: string = '';
+
+        private _startDate: Date = new Date();
+
+        constructor(title: string, message: string, type: string) {
+            this._title = title;
+            this._message = message;
+            this._type = type
         }
-        public end() {
-            console.timeEnd(this.msg);
+
+        public end(): void {
+            let endDate: Date = new Date()
+            
+            PrintService.log(`${!this._title ? '' : `<${this._title}> `}${this._message} in ${endDate.getTime() - this._startDate.getTime()}ms`, undefined, this._type as any);
         }
     }
 
-    let timeCount = 0;
-    export function TraceTime(title: string, message: string) {
+    export function TraceTime(title: string, message: string): TimeEndWrapper {
         if (!TestPass(Level.Trace)) return;
-        let msg = getTraceMessage(title, message, true) + `(#${++timeCount})`;
-        console.time(msg);
-        return new TimeEndWrapper(msg);
+        
+        return new TimeEndWrapper(title, message, 'message');
     }
 
-    export function InfoTime(title: string, message: string) {
+    export function InfoTime(title: string, message: string): TimeEndWrapper {
         if (!TestPass(Level.Info)) return;
-        let msg = getInfoMessage(title, message, true) + `(#${++timeCount})`;
-        console.time(msg);
-        return new TimeEndWrapper(msg);
+
+        return new TimeEndWrapper(title, message, 'info');
     }
 
-    // export function InfoTimeEnd(title: string, message: string) {
-    //     if (!TestPass(Level.Info)) return;
-    //     let msg = getInfoMessage(title, message, false) + `(#${++timeCount})`;
-    //     console.timeEnd(msg);
-    //     return new TimeEndWrapper(msg);
-    // }
+    export function SuccessTime(title: string, message: string): TimeEndWrapper {
+        if (!TestPass(Level.Success)) return;
+
+        return new TimeEndWrapper(title, message, 'success');
+    }
 }

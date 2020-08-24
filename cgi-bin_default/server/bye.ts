02b759808d8e24e1afd64894ce09cc357ad43cf6
Min.Hsieh <Min.Hsieh@isapsolution.com>
2020-08-24 17:59:46 +0800

    Upgrade Server Bye cgi add post action

--- cgi-bin_default/server/bye.ts
+++ cgi-bin_default/server/bye.ts
@@ -3,11 +3,34 @@ import {} from 'helpers';
 import { BasicAuth } from 'helpers/middlewares';
 import * as Permission from 'workspace/define/userRoles/userPermission.define';
 
-let action = new Action({
-    loginRequired: false,
-});
+let action = new Action({});
 
 export default action;
+
+/**
+ * Action Post
+ */
+type InputC = null;
+
+type OutputC = string;
+
+action.post(
+    {
+        loginRequired: true,
+        permission: [RoleList.SystemAdministrator],
+    },
+    async (data): Promise<OutputC> => {
+        try {
+            setTimeout(() => {
+                process.exit(1);
+            }, 0);
+
+            return 'bye';
+        } catch (e) {
+            throw e;
+        }
+    },
+);
 
 /**
  * Action Get
@@ -18,6 +41,7 @@ type OutputR = string;
 
 action.get(
     {
+        loginRequired: false,
         middlewares: [BasicAuth(Permission['permissionServerByeR'])],
     },
     async (data): Promise<OutputR> => {

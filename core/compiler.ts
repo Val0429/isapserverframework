5833d94c9812caa3e4112754d6638c708ad75ec2
Min.Hsieh <Min.Hsieh@isapsolution.com>
2020-11-27 14:32:21 +0800

    Upgrade Compiler core udpate process of loading

--- core/compiler.ts
+++ core/compiler.ts
@@ -7,9 +7,9 @@
 import 'shells/package.shell';
 
 import 'shells/config.shell';
-
-import 'shells/server.shell';
 import 'shells/userRoles.shell';
 import 'shells/personRoles.shell';
 import 'shells/errors.shell';
 import 'shells/licenses.shell';
+
+import 'shells/server.shell';

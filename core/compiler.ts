36bce771028a4353c61db1fc201f3faeb2a8d8fa
Mohammad Firdaus Ardiansyah <mfirdaus2007@gmail.com>
2021-05-11 09:43:31 +0800

    Fix license definition error

--- core/compiler.ts
+++ core/compiler.ts
@@ -6,10 +6,11 @@
 
 import 'shells/package.shell';
 
-import 'shells/config.shell';
 import 'shells/userRoles.shell';
 import 'shells/personRoles.shell';
 import 'shells/errors.shell';
 import 'shells/licenses.shell';
+
+import 'shells/config.shell';
 
 import 'shells/server.shell';

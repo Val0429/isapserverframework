bdf98b31a4b0a994be6cea662d1cc9abb9885deb
Val.Liu <Val.Liu@isapsolution.com>
2021-09-03 11:32:23 +0800

    update shell for windows

--- shells/archive.shell.ts
+++ shells/archive.shell.ts
@@ -1,6 +1,7 @@
 import { execSync } from 'child_process';
 import * as path from 'path';
 import * as fs from 'fs';
+const isWindows = process.platform === "win32";
 
 let args = process.argv.splice(2, process.argv.length);
 let custompath: string = undefined;
@@ -15,6 +16,10 @@ let packagepath = path.resolve(__dirname, "../");
 let packagejsonpath = path.resolve(packagepath, "workspace", "package.json");
 let packagejson = require(packagejsonpath);
 
-let cwd = path.resolve(packagepath, custompath);
-
-let result = execSync(`git archive --output=${packagejson.name}.zip HEAD $(git diff --name-only ${from} ${to})`, { cwd });
+let cwd = path.resolve(packagepath, custompath || "./");
+let gitBashPath = "C:\\Program Files\\Git\\bin\\bash.exe";
+let shell: string = isWindows ? (fs.existsSync(gitBashPath) ? gitBashPath : undefined) : undefined;
+let result = execSync(`git archive --output=${packagejson.name}.zip HEAD $(git diff --name-only ${from} ${to})`, {
+    cwd,
+    shell
+});
